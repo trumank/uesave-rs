fb9363fb14a1219704cd28b01606a8734255c4a5
Truman Kilen <t@kilen.me>
2023-01-26 22:34:56 -0600

    cargo fmt

--- examples/space-rig-decorator/main.rs
+++ examples/space-rig-decorator/main.rs
@@ -2,44 +2,46 @@ use std::fs::File;
 
 use anyhow::Result;
 
-use uesave::{Save, PropertyMeta, ValueArray, ValueStruct, ValueBase};
+use uesave::{PropertyMeta, Save, ValueArray, ValueBase, ValueStruct};
 
 fn main() -> Result<()> {
-    let save = Save::read(&mut File::open("examples/space-rig-decorator/PropPack.sav")?)?;
+    let save = Save::read(&mut File::open(
+        "examples/space-rig-decorator/PropPack.sav",
+    )?)?;
     let props = &save.root.root[0].value;
-    if let PropertyMeta::Array{value, ..} = props {
-        if let ValueArray::Struct{value, ..} = value {
+    if let PropertyMeta::Array { value, .. } = props {
+        if let ValueArray::Struct { value, .. } = value {
             for prop in value {
                 if let ValueStruct::Struct(p) = prop {
-                    if let PropertyMeta::Struct{value, ..} = &p[0].value {
+                    if let PropertyMeta::Struct { value, .. } = &p[0].value {
                         if let ValueStruct::Struct(value) = value {
-                            if let PropertyMeta::Struct{value, ..} = &value[0].value {
+                            if let PropertyMeta::Struct { value, .. } = &value[0].value {
                                 if let ValueStruct::Base(ValueBase::Quat(value)) = value {
                                     print!("{}:{}:{}:{}:", value.x, value.y, value.z, value.w);
                                 }
                             }
-                            if let PropertyMeta::Struct{value, ..} = &value[1].value {
+                            if let PropertyMeta::Struct { value, .. } = &value[1].value {
                                 if let ValueStruct::Base(ValueBase::Vector(value)) = value {
                                     print!("{}:{}:{}:", value.x, value.y, value.z);
                                 }
                             }
-                            if let PropertyMeta::Struct{value, ..} = &value[2].value {
+                            if let PropertyMeta::Struct { value, .. } = &value[2].value {
                                 if let ValueStruct::Base(ValueBase::Vector(value)) = value {
                                     print!("{}:{}:{}:", value.x, value.y, value.z);
                                 }
                             }
                         }
                     }
-                    if let PropertyMeta::Str{value, ..} = &p[1].value {
+                    if let PropertyMeta::Str { value, .. } = &p[1].value {
                         print!("{value}:");
                     }
-                    if let PropertyMeta::Bool{value, ..} = &p[2].value {
+                    if let PropertyMeta::Bool { value, .. } = &p[2].value {
                         print!("{value}:");
                     }
-                    if let PropertyMeta::Object{value, ..} = &p[3].value {
+                    if let PropertyMeta::Object { value, .. } = &p[3].value {
                         print!("{value}:");
                     }
-                    if let PropertyMeta::Object{value, ..} = &p[4].value {
+                    if let PropertyMeta::Object { value, .. } = &p[4].value {
                         println!("{value}");
                     }
                 }
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,44 @@
 use std::fs::{File, OpenOptions};
-use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Write, Cursor};
+use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Cursor, Write};
 
-use clap::{Parser, Subcommand};
 use anyhow::Result;
+use clap::{Parser, Subcommand};
 
 use uesave::Save;
 
 #[derive(Parser, Debug)]
 struct IO {
-   #[arg(short, long, default_value = "-")]
-   input: String,
+    #[arg(short, long, default_value = "-")]
+    input: String,
 
-   #[arg(short, long, default_value = "-")]
-   output: String,
+    #[arg(short, long, default_value = "-")]
+    output: String,
 }
 
 #[derive(Parser, Debug)]
 struct Edit {
-   #[arg(required = true, index = 1)]
-   path: String,
+    #[arg(required = true, index = 1)]
+    path: String,
 
-   #[arg(short, long)]
-   editor: Option<String>,
+    #[arg(short, long)]
+    editor: Option<String>,
 }
 
 #[derive(Subcommand, Debug)]
 enum Action {
-   /// Convert binary save to plain text JSON
-   ToJson(IO),
-   /// Convert JSON back to binary save
-   FromJson(IO),
-   /// Launch $EDITOR to edit a save file as JSON in place
-   Edit(Edit),
+    /// Convert binary save to plain text JSON
+    ToJson(IO),
+    /// Convert JSON back to binary save
+    FromJson(IO),
+    /// Launch $EDITOR to edit a save file as JSON in place
+    Edit(Edit),
 }
 
 #[derive(Parser, Debug)]
 #[command(author, version)]
 struct Args {
-   #[command(subcommand)]
-   action: Action,
+    #[command(subcommand)]
+    action: Action,
 }
 
 pub fn main() -> Result<()> {
@@ -48,15 +48,15 @@ pub fn main() -> Result<()> {
         Action::ToJson(io) => {
             let save = Save::read(&mut input(&io.input)?)?;
             serde_json::to_writer(output(&io.output)?, &save)?;
-        },
+        }
         Action::FromJson(io) => {
             let save: Save = serde_json::from_reader(&mut input(&io.input)?)?;
             save.write(&mut output(&io.output)?)?;
-        },
+        }
         Action::Edit(edit) => {
             let editor = match edit.editor {
                 Some(editor) => editor,
-                None => std::env::var("EDITOR").unwrap_or_else(|_| "vim".to_string())
+                None => std::env::var("EDITOR").unwrap_or_else(|_| "vim".to_string()),
             };
 
             // read and parse save file
@@ -65,9 +65,7 @@ pub fn main() -> Result<()> {
             let value = serde_json::to_value(save)?;
 
             // create temp file and write formatted JSON to it
-            let temp = tempfile::Builder::new()
-                .suffix(".json")
-                .tempfile()?;
+            let temp = tempfile::Builder::new().suffix(".json").tempfile()?;
             serde_json::to_writer_pretty(BufWriter::new(&temp), &value)?;
 
             // launch editor
@@ -92,33 +90,27 @@ pub fn main() -> Result<()> {
                     .open(edit.path)?
                     .write_all(&out_buffer)?;
             }
-        },
+        }
     }
     Ok(())
 }
 
 fn input<'a>(path: &str) -> Result<Box<dyn BufRead + 'a>> {
     Ok(match path {
-        "-" => {
-            Box::new(BufReader::new(stdin().lock()))
-        },
-        p => {
-            Box::new(BufReader::new(File::open(p)?))
-        },
+        "-" => Box::new(BufReader::new(stdin().lock())),
+        p => Box::new(BufReader::new(File::open(p)?)),
     })
 }
 
 fn output<'a>(path: &str) -> Result<Box<dyn Write + 'a>> {
     Ok(match path {
-        "-" => {
-            Box::new(BufWriter::new(stdout().lock()))
-        },
-        p => {
-            Box::new(BufWriter::new(OpenOptions::new()
+        "-" => Box::new(BufWriter::new(stdout().lock())),
+        p => Box::new(BufWriter::new(
+            OpenOptions::new()
                 .create(true)
                 .truncate(true)
                 .write(true)
-                .open(p)?))
-        }
+                .open(p)?,
+        )),
     })
 }
